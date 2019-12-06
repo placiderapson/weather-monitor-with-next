90e11b63b622b372a0a91f4294af3fec54aedab2
Placide IRANDORA <placideirandora@gmail.com>
2019-12-07 00:03:37 +0200

    Render the Search component

--- components/App.jsx
+++ components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Search from './shared/Search';
 import '../styles/App.scss';
 
 const App = () => {
     return(
         <div className="App">
-           <div className="App__left-section"></div>
+           <div className="App__left-section">
+               <Search />
+           </div>
            <div className="App__right-section"></div>
         </div>
     )
