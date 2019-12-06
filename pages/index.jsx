e9f033914546260379f9eafdb2e74a4d1b3c6b95
Placide IRANDORA <placideirandora@gmail.com>
2019-12-06 20:51:57 +0200

    Add initial homepage for starting off

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const Homepage = () => {
+    return(
+        <Fragment>
+            <h1>WEATHER MONITOR</h1>
+        </Fragment>
+    );
+};
+
+export default Homepage;
