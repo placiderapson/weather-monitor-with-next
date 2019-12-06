2a7e4a2d728163246806ec17ad6e2bdebc36738b
Placide IRANDORA <placideirandora@gmail.com>
2019-12-06 21:45:59 +0200

    Improve the homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,15 @@
 import React, { Fragment } from 'react';
+import Head from 'next/head';
+import '../styles/Homepage.scss';
 
 const Homepage = () => {
     return(
         <Fragment>
-            <h1>WEATHER MONITOR</h1>
+            <Head>
+                <title>{ 'WEATHER MONITOR' }</title>
+                <meta charSet='utf-8' />
+                <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+            </Head>
         </Fragment>
     );
 };
