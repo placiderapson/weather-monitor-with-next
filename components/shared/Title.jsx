c3c2eca16c0223ad3c4ab2788c8998f3fa57bec6
Placide IRANDORA <placideirandora@gmail.com>
2019-12-07 00:24:40 +0200

    Add Title component

---
+++ components/shared/Title.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../styles/Title.scss';
+
+const Title = () => {
+    return(
+        <div className="Title">
+            <p className="Title__first-word">Weather</p>
+            <p className="Title__second-word">Monitor</p>
+        </div>
+    );
+};
+
+export default Title;
