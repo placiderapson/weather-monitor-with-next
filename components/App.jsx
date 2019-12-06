112357a682bec5955cb9ca6735257f7341b539e3
Placide IRANDORA <placideirandora@gmail.com>
2019-12-07 01:54:22 +0200

    Render the locations

--- components/App.jsx
+++ components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Search from './shared/Search';
 import Title from './shared/Title';
+import Location from './shared/Location';
 import '../styles/App.scss';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
            <div className="App__left-section">
                <Search />
                <Title />
+               <Location />
            </div>
            <div className="App__right-section"></div>
         </div>
