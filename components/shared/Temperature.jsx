2835be9b679a20b5f37020a35ae4a5c32f225c11
Placide IRANDORA <placideirandora@gmail.com>
2019-12-07 23:43:43 +0200

    Display the temperature that matches the current location

--- components/shared/Temperature.jsx
+++ components/shared/Temperature.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../App';
 import '../../styles/Temperature.scss';
 
 const Temperature = () => {
+    const context = useContext(AppContext);
+
     return(
         <div className="Temperature">
-            <p className="Temperature__value">28</p>
+            <p className="Temperature__value">{ context.state.currentTemperature }</p>
             <p className="Temperature__unit"><sup> &#8451;</sup></p>
         </div>
     );
