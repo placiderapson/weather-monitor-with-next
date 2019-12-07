de6446b592e9650ba8d28377ca727a29adbb673d
Placide IRANDORA <placideirandora@gmail.com>
2019-12-07 14:53:40 +0200

    Add Today component for displaying the current date

---
+++ components/shared/Today.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../styles/Today.scss';
+
+const Today = () => {
+    return(
+        <div className="Today">
+            <h2 className="Today__title">Today</h2>
+            <p className="Today__date">Sat, 3 Aug</p>
+        </div>
+    );
+};
+
+export default Today;
