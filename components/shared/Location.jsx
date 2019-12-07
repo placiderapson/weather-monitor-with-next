80fed8d65616d478cc85422fda5195ef6f8e20c3
Placide IRANDORA <placideirandora@gmail.com>
2019-12-07 22:42:40 +0200

    Refactor to use Reducer for updating the state

--- components/shared/Location.jsx
+++ components/shared/Location.jsx
@@ -5,24 +5,28 @@ import '../../styles/Location.scss';
 const Location = () => {
     const context = useContext(AppContext);
 
+    const setCurrentCity = (city) => {
+        context.dispatch({ type: 'CHANGE-LOCATION', payload: city  });
+    }
+
     return(
         <div className="Location">
-            <div className="Location__card">
+            <div className="Location__card" onClick={()=>{setCurrentCity(context.state.cities[0])}}>
                 <div className="Location__landmark-one Location__landmark--active" />
                 <div className="Location__country-city">
-                    <p>{ context.cities[0] }</p>
+                    <p>{ context.state.cities[0] }</p>
                 </div>
             </div>
-            <div className="Location__card">
+            <div className="Location__card" onClick={()=>{setCurrentCity(context.state.cities[1])}}>
                 <div className="Location__landmark-two Location__landmark--inactive" />
                 <div className="Location__country-city">
-                <p>{ context.cities[1] }</p>
+                <p>{ context.state.cities[1] }</p>
                 </div>
             </div>
-            <div className="Location__card">
+            <div className="Location__card" onClick={()=>{setCurrentCity(context.state.cities[2])}}>
                 <div className="Location__landmark-three Location__landmark--inactive" />
                 <div className="Location__country-city">
-                <p>{ context.cities[2] }</p>
+                <p>{ context.state.cities[2] }</p>
                 </div>
             </div>
         </div>
