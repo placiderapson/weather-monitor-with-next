dc2494d4e1dc4711c34da6d418c963e1e35eb9d1
Placide IRANDORA <placideirandora@gmail.com>
2019-12-07 15:03:36 +0200

    Add City component for displaying selected city's temperature

---
+++ components/shared/City.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/City.scss';
+
+const City = () => {
+    return(
+        <div className="City">
+            <p>Berlin, Germany</p>
+        </div>
+    );
+};
+
+export default City;
