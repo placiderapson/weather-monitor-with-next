da94862eb10537a362021325376f73949c140974
Placide IRANDORA <placideirandora@gmail.com>
2019-12-07 16:35:09 +0200

    Handle the location search

--- components/shared/Search.jsx
+++ components/shared/Search.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/Search.scss';
 
 const Search = () => {
+    const [ location, setLocation ] = useState('');
+    const [ message, setMessage ] = useState('');
+
+    const handleOnChange = (e) => {
+        setLocation(e.target.value);
+    }
+    
+    const handleOnClick = (e) => {
+        e.preventDefault();
+        location.trim() ? setMessage(`Weather Forecast For ${location.toUpperCase()} Could Not Be Found`) : null;
+        location.trim() ? setTimeout(()=> {
+            setMessage('')
+        }, 5000) : null;
+    }
+
     return(
         <div className="Search">
-            <input type="text" placeholder="Search a new place" className="Search__input"/>
-            <button className="Search__btn">Search</button>
+            <input type="text" placeholder="Search a new place" className="Search__input" onChange={ handleOnChange }/>
+            <button className="Search__btn" onClick={ handleOnClick }>Search</button>
+            <p className="Search__message">{ message }</p>
         </div>
     );
 };
