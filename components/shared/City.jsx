6e1188ebf947a940ea6b731a9aee93b7edfd1ad8
Placide IRANDORA <placideirandora@gmail.com>
2019-12-07 20:11:30 +0200

    Use shared App data instead of hard-coded one

--- components/shared/City.jsx
+++ components/shared/City.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../App';
 import '../../styles/City.scss';
 
 const City = () => {
+    const context = useContext(AppContext);
+
     return(
         <div className="City">
-            <p>Berlin, Germany</p>
+            <p>{ context.cities[0] }</p>
         </div>
     );
 };
--- components/shared/Location.jsx
+++ components/shared/Location.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../App';
 import '../../styles/Location.scss';
 
 const Location = () => {
+    const context = useContext(AppContext);
+
     return(
         <div className="Location">
             <div className="Location__card">
                 <div className="Location__landmark-one Location__landmark--active" />
                 <div className="Location__country-city">
-                    <p>Berlin, Germany</p>
+                    <p>{ context.cities[0] }</p>
                 </div>
             </div>
             <div className="Location__card">
                 <div className="Location__landmark-two Location__landmark--inactive" />
                 <div className="Location__country-city">
-                    <p>Paris, France</p>
+                <p>{ context.cities[1] }</p>
                 </div>
             </div>
             <div className="Location__card">
                 <div className="Location__landmark-three Location__landmark--inactive" />
                 <div className="Location__country-city">
-                    <p>New York, USA</p>
+                <p>{ context.cities[2] }</p>
                 </div>
             </div>
         </div>
