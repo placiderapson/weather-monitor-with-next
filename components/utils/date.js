55d4c625dc6491dfa5b55e12cb8b7bba8f4df4b1
Placide IRANDORA <placideirandora@gmail.com>
2019-12-07 20:42:44 +0200

    Use single comma

--- components/utils/date.js
+++ components/utils/date.js
@@ -1,3 +1,3 @@
 const options = { weekday: 'short', month: 'short', day: 'numeric' };
 
-export const date = new Date().toLocaleString("en-US", options);
+export const date = new Date().toLocaleString('en-US', options);
