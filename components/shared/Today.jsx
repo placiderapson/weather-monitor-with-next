2daacafa4d12c1f09fc93e9d0d87db73160ea584
Placide IRANDORA <placideirandora@gmail.com>
2019-12-07 20:41:20 +0200

    Use dynamic App data instead of hard-coded one

--- components/shared/Today.jsx
+++ components/shared/Today.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../App';
 import '../../styles/Today.scss';
 
 const Today = () => {
+    const context = useContext(AppContext);
+
     return(
         <div className="Today">
             <h2 className="Today__title">Today</h2>
             <div className="Today__container">
-                <p className="Today__date">Sat, 3 Aug</p>
-                <p className="Today__forecast">Cloudy</p>
+                <p className="Today__date">{ context.today }</p>
+                <p className="Today__forecast">{ context.weatherForecast[0] }</p>
             </div>
         </div>
     );
