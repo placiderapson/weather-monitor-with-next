bffc3796854d08868f7f92c7ea7c5c2ca06b6373
Placide IRANDORA <placideirandora@gmail.com>
2019-12-07 20:10:44 +0200

    Configure the App to use Context API for state management

--- components/App.jsx
+++ components/App.jsx
@@ -5,23 +5,28 @@ import Location from './shared/Location';
 import Today from './shared/Today';
 import Temperature from './shared/Temperature';
 import City from './shared/City';
+import { AppData } from './data/App';
 import '../styles/App.scss';
+
+export const AppContext = React.createContext();
 
 const App = () => {
     return(
-        <div className="App">
-           <div className="App__left-section">
-               <Search />
-               <Title />
-               <Location />
-           </div>
-           <div className="App__right-section">
-               <Today />
-               <Temperature />
-               <City />
-           </div>
-        </div>
-    )
-}
+        <AppContext.Provider value={AppData}>
+            <div className="App">
+                <div className="App__left-section">
+                    <Search />
+                    <Title />
+                    <Location />
+                </div>
+                <div className="App__right-section">
+                    <Today />
+                    <Temperature />
+                    <City />
+                </div>
+            </div>
+        </AppContext.Provider>
+    );
+};
 
 export default App;
