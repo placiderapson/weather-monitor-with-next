76ed90e57823b35df8d4bf0cb54d18b0315386f8
Placide IRANDORA <placideirandora@gmail.com>
2019-12-07 20:40:18 +0200

    Add more data

--- components/data/App.js
+++ components/data/App.js
@@ -1,3 +1,7 @@
+import { date } from '../utils/date';
+
 export const AppData = {
+    today: date,
+    weatherForecast: ['cloudy', 'sunny', 'rainy'],
     cities: ['Berlin, Germany', 'Paris, France', 'New York, USA']
 };
