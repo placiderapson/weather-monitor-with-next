5740c37f215c86bfec5b2113606d980e804bb85c
Placide IRANDORA <placideirandora@gmail.com>
2019-12-08 02:32:21 +0200

    Display success or error message accordingly

--- components/shared/Search.jsx
+++ components/shared/Search.jsx
@@ -3,45 +3,57 @@ import { AppContext } from '../App';
 import '../../styles/Search.scss';
 
 const Search = () => {
-    const context = useContext(AppContext);
+  const context = useContext(AppContext);
 
-    const [ location, setLocation ] = useState('');
-    const [ message, setMessage ] = useState('');
+  const [location, setLocation] = useState('');
+  const [message, setMessage] = useState('');
+  const [style, setStyle] = useState('');
 
-    const handleOnChange = (e) => {
-        setLocation(e.target.value);
-    }
-    
-    const handleOnClick = e => {
-      e.preventDefault();
-      let city;
-      location.trim()
-        ? (city = context.state.cities.filter(
-            city => city === location.toLowerCase()
-          ))
-        : null;
-      location.trim()
-        ? setMessage(
-            city.length
-              ? `Weather Forecast Found. Click On ${city[0].toUpperCase()} Below`
-              : `Weather Forecast For ${location.toUpperCase()} Could Not Be Found`
+  const handleOnChange = e => {
+    setLocation(e.target.value);
+  };
+
+  const handleOnClick = e => {
+    e.preventDefault();
+    let city;
+    location.trim()
+      ? (city = context.state.cities.filter(
+          city => city === location.toLowerCase()
+        ))
+      : null;
+    location.trim()
+      ? city.length
+        ? setStyle('Search__success-message') &
+          setMessage(
+            `Weather Forecast Found. Click On ${city[0].toUpperCase()} Below`
           )
-        : null;
+        : setStyle('Search__error-message') &
+          setMessage(
+            `Weather Forecast For ${location.toUpperCase()} Could Not Be Found`
+          )
+      : null;
 
-      location.trim()
-        ? setTimeout(() => {
-            setMessage("");
-          }, 5000)
-        : null;
-    };
+    location.trim()
+      ? setTimeout(() => {
+          setMessage('');
+        }, 5000)
+      : null;
+  };
 
-    return(
-        <div className="Search">
-            <input type="text" placeholder="Search a new place. Ex: city, country" className="Search__input" onChange={ handleOnChange }/>
-            <button className="Search__btn" onClick={ handleOnClick }>Search</button>
-            <p className="Search__message">{ message }</p>
-        </div>
-    );
+  return (
+    <div className="Search">
+      <input
+        type="text"
+        placeholder="Search a new place. Ex: city, country"
+        className="Search__input"
+        onChange={handleOnChange}
+      />
+      <button className="Search__btn" onClick={handleOnClick}>
+        Search
+      </button>
+      <p className={style}>{message}</p>
+    </div>
+  );
 };
 
 export default Search;
